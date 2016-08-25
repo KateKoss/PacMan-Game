141c008ecb7191e7133ceb0be9d708080f5601b8
KateKoss <kos.ka@outlook.com>
2016-08-25 16:26:19 +0300

    Введён враг, передвижение врага

--- PacMan/main.cpp
+++ PacMan/main.cpp
@@ -51,6 +51,148 @@ float X = 17, Y = 17;	//координати персонажа
 
 float currentFrame = 0;
 
+class Enemy
+{
+private:
+	float dxEnemy, dyEnemy;
+	float XEnemy, YEnemy;	//координати врага
+	int currentFrame;
+	bool alive;
+public:
+	Sprite enemySprite;
+	Enemy(Texture &enemyTexture, int X, int Y) //текстура и начальные координаты пложения врага на карте
+	{
+		XEnemy = X;
+		YEnemy = Y;
+		enemySprite.setTexture(enemyTexture);//загружаем текстуру в спрайт
+		enemySprite.setTextureRect(IntRect(457,65,13,13)); //вырезаем картинку героя из тайлсета
+		enemySprite.setPosition(X, Y);//устанавливаем начальное положение врага на карте
+		currentFrame = dxEnemy = dyEnemy = 0;
+	};
+	void update(float time)
+	{
+		findEnemyDirection();
+		XEnemy += dxEnemy*time;
+		Collision(0);
+
+		YEnemy += dyEnemy*time;
+		Collision(1);
+		
+		if ((YEnemy / ts.height) > 0 && (XEnemy / ts.width) > 0)
+		enemySprite.setPosition(XEnemy, YEnemy);
+		dxEnemy = dyEnemy = 0;
+	}
+	void Collision(int dir)
+	{
+		if ((XEnemy / ts.width) > 0 && (YEnemy / ts.height) > 0)
+		{
+			for (int i = YEnemy / ts.height; i < (YEnemy + ps.height) / ts.height; i++)
+			{
+				for (int j = XEnemy / ts.width; j < (XEnemy + ps.width) / ts.width; j++)
+				{
+					if (TileMap[i][j] == '0')
+					{
+						if (dxEnemy > 0 && dir == 0)
+							XEnemy = j * ts.width - ps.width;
+
+						if (dxEnemy < 0 && dir == 0)
+							XEnemy = j * ts.width + ts.width;
+
+						//если dir == 1 то мы двигаемся по оси 0Х
+						if (dyEnemy > 0 && dir == 1)
+						{
+							YEnemy = i * ts.height - ps.height;
+							dyEnemy = 0;
+						}
+						if (dyEnemy < 0 && dir == 1)
+						{
+							YEnemy = i * ts.height + ts.height;
+							dyEnemy = 0;
+						}
+					}
+				}
+			}
+		}
+	}
+	void findEnemyDirection()
+	{
+		if (X >= XEnemy)
+		{
+			if (Y < YEnemy)
+			{
+				if (X == XEnemy)
+				{
+					dyEnemy -= 0.1;//8
+				}
+				else
+				{
+					dxEnemy += 0.1;//4
+					dyEnemy -= 0.1;
+				}
+			}
+			else
+			{
+				if (X == XEnemy)
+				{
+					dyEnemy += 0.1;//6
+				}
+				else
+				{
+					dxEnemy += 0.1;
+					dyEnemy += 0.1;//1
+				}
+			}
+		}
+		else
+		{
+			if (Y >= YEnemy)
+			{
+				if (Y == YEnemy)
+				{
+					dxEnemy -= 0.1; //7
+				}
+				else
+				{
+					dxEnemy -= 0.1;//2
+					dyEnemy += 0.1;
+				}
+			}
+			else
+			{
+				if (Y == YEnemy)
+				{
+					dxEnemy -= 0.1;//5
+				}
+				else
+				{
+					dxEnemy -= 0.1;//3
+					dyEnemy -= 0.1;
+				}
+			}
+		}
+		/*if (X > XEnemy && Y > YEnemy)
+		{
+			dxEnemy += 0.1;
+			dyEnemy += 0.1;
+		}
+		else if(X < XEnemy && Y > YEnemy)
+		{
+			dxEnemy -= 0.1;
+			dyEnemy += 0.1;
+		}
+		else if(X < XEnemy && Y < YEnemy)
+		{
+			dxEnemy -= 0.1;
+			dyEnemy -= 0.1;
+		}
+		else if (X > XEnemy && Y < YEnemy)
+		{
+			dxEnemy += 0.1;
+			dyEnemy -= 0.1;
+		}*/
+	}
+};
+
 void Collision(int dir)
 {	
 	if ((Y / ts.height) > 0 && (X / ts.width) > 0)
@@ -96,21 +238,24 @@ void update(float time)
 	Collision(1);
 	
 	if ((Y / ts.height) > 0 && (X / ts.width) > 0)
-	heroSprite.setPosition(X, Y);
-	dx = 0;
+		heroSprite.setPosition(X, Y);
+	//dx = 0;		//тут герой начинает сам двигаться после нажатия стрелки движения
+	//dy = 0;
 }
 
 int main(int argc, char *argv[])
 {
 	RenderWindow window(sf::VideoMode(W*ts.width, H*ts.height), "PacMan"); //,Style::Fullscreen
+	
 	Image heroImage;
-	heroImage.loadFromFile("images/Pac_Man_Sprites.png");
+	heroImage.loadFromFile("images/Pac_Man_Sprites1.png");
 	
 	Texture heroTexture;
 	heroTexture.loadFromImage(heroImage);
+	//------------------------------
 	Texture t;
 	t.loadFromFile("images/Map.png");
-
+	
 	Sprite s;
 	s.setTexture(t);
 	s.setTextureRect(IntRect(0, 0, 304, 350));
@@ -119,13 +264,15 @@ int main(int argc, char *argv[])
 	heroSprite.setTexture(heroTexture);
 	heroSprite.setTextureRect(IntRect(455, 0, ps.width, ps.height));
 	heroSprite.setPosition(X, Y);
-	
+	//------------------------- это нужно загнать в методы
 	Texture mapTexture;
 	mapTexture.loadFromFile("images/Pac_Man_Sprites1.png");
 	Sprite mapSprite;	
 	mapSprite.setTexture(mapTexture);
 	mapSprite.setTextureRect(IntRect(614, 20, 16, 16));//607, 21, 16, 16//ts.width, ts.height
-	
+	//---------------------
+	Enemy enemy(mapTexture,17*1,17*2);
+
 	Clock clock;
 
 	RectangleShape rectangle(Vector2f(ts.width, ts.height));
@@ -221,8 +368,7 @@ int main(int argc, char *argv[])
 			dy = 0.1;
 			dx = 0;
 		}	
-		//window.setIcon()
-		window.clear(Color::Blue);
+		window.clear(Color::Blue); //нам незачем очищать весь экран, достаточно удалять героя
 
 		for (int i = 0; i < H; i++)
 		{
@@ -246,9 +392,11 @@ int main(int argc, char *argv[])
 				rectangle.setPosition(j * ts.width, i * ts.height);
 				window.draw(rectangle);
 			}
+			window.draw(enemy.enemySprite);	//рисуем врага
 		}
 		
 		update(time);
+		enemy.update(time*0.5);
 		//window.draw(s); //если убрать комментарии наложится карта
 		window.draw(heroSprite);
 		
